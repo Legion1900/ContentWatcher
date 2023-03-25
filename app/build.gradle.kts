aa3ef2f3136efccdb5df17c61d0e63fb36060511
Roman Chumachenko <chumachenko.roman@airslate.com>
2023-03-25 18:17:53 +0200

    Migrated dependencies to version catalog.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.app)
+    alias(libs.plugins.android.kotlin)
 }
 
 android {
@@ -32,8 +32,7 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.google.android.material)
 }
