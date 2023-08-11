0c05f5298b0af42ff2ba6a2592f991c59d5ed710
Roman Chumachenko <chumachenko.roman@airslate.com>
2023-08-11 15:17:50 +0300

    Added caching of game data.

--- game_list_feature/src/main/java/com/legion1900/game_list_feature/providers/GamesRepository.kt
+++ game_list_feature/src/main/java/com/legion1900/game_list_feature/providers/GamesRepository.kt
@@ -1,17 +1,35 @@
 package com.legion1900.game_list_feature.providers
 
+import com.legion1900.base.exceptions.ConnectivityException
 import com.legion1900.game_list_feature.models.GameCover
-import com.legion1900.network.services.wrappers.IGDBService
+import com.legion1900.game_list_feature.models.GameDetails
 
 class GamesRepository internal constructor(
     private val remoteGameSource: RemoteGameSource,
-    private val gameStorage: GameStorage
+    private val localGameSource: GameStorage
 ) {
-    suspend fun getGameCovers(offset: Int, limit: Int): List<GameCover> {
-        return remoteGameSource.getGameDetails(offset, limit)
-            .map { details ->
-                val cover = details.screenshot.lastOrNull() ?: details.cover ?: details.artworks.firstOrNull()
-                GameCover(details.id, cover, details.name)
+
+    suspend fun getGameCovers(
+        offset: Int,
+        limit: Int
+    ): List<GameCover> {
+        var gameDetails = localGameSource.getGameDetails(offset, limit)
+        if (gameDetails.isEmpty()) {
+            try {
+                gameDetails = remoteGameSource.getGameDetails(offset, limit)
+                localGameSource.storeGames(gameDetails)
+            } catch (e: ConnectivityException) {
+                gameDetails = emptyList()
             }
+        }
+
+        return gameDetails.toCover()
+    }
+
+    private fun List<GameDetails>.toCover(): List<GameCover> {
+        return map { details ->
+            val cover = details.screenshot.lastOrNull() ?: details.cover ?: details.artworks.firstOrNull()
+            GameCover(details.id, cover, details.name)
+        }
     }
 }
