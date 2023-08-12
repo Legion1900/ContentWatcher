079ee48ca295fb0d69a44d79b880e0f3a174c8ba
Roman Chumachenko <chumachenko.roman@airslate.com>
2023-08-12 18:20:50 +0300

    Fine tuned request for game list.

--- network/src/main/java/com/legion1900/network/services/wrappers/IGDBBodyTemplates.kt
+++ network/src/main/java/com/legion1900/network/services/wrappers/IGDBBodyTemplates.kt
@@ -1,7 +1,7 @@
 package com.legion1900.network.services.wrappers
 
 internal object IGDBBodyTemplates {
-    const val GAME_LIST_BODY = "fields name, summary, category, first_release_date, genres; offset %d; limit %d;"
+    const val GAME_LIST_BODY = "fields name, summary, category, first_release_date, genres; where aggregated_rating > 75 & screenshots > 3 & summary != null & first_release_date != null; sort first_release_date desc; offset %d; limit %d;"
     const val GAME_ART_BODY = "fields game, image_id; where game = %d;"
     const val GAME_SCREENSHOT_BODY = "fields game, image_id; where game = %d;"
     const val GAME_COVER_BODY = "fields image_id; where game = %d;"
