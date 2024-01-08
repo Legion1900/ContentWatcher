b3de283074e43d260a64f7826e0ad8195e36d1dc
Roman Chumachenko <chumachenko.roman@airslate.com>
2024-01-08 16:16:03 +0200

    Cleared logging.

--- network/src/main/java/com/legion1900/network/auth/IGDBAuthInterceptor.kt
+++ network/src/main/java/com/legion1900/network/auth/IGDBAuthInterceptor.kt
@@ -1,6 +1,5 @@
 package com.legion1900.network.auth
 
-import android.util.Log
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Request
@@ -10,14 +9,12 @@ private const val IGDB_HEADER_CLIENT_ID = "Client-ID"
 private const val IGDB_HEADER_AUTHORIZATION = "Authorization"
 
 private const val AUTH_ERROR_CODE = 401
-private const val TOO_MANY_REQUESTS_ERROR_CODE = 401
 
 internal class IGDBAuthInterceptor(
     private val authHandler: IGDBAuthHandler,
 ) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        Log.d("interceptorSpecial", "${Thread.currentThread().id}")
         return runBlocking {
             val request = buildAuthenticatedRequest(chain)
             runRequest(chain, request)
